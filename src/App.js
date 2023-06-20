c86509e1b362a9986a9e1941630f1dbd7a6b989a
Luke-Cragg <124095690+Luke-Cragg@users.noreply.github.com>
2023-06-20 20:06:36 +0100

    Changing website title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <head>
+        <title>LComputing</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="stylesheet"
